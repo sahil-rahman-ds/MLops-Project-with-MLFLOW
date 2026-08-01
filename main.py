922af5f969089e2b2f721277dfabf9c910aaaa9d
sahil-rahman-ds <sahilrahman0827@gmail.com>
2026-08-01 23:24:09 +0530

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlProject import logger
-from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline, STAGE_NAME
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -11,4 +12,15 @@ try:
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
+    raise e
+
+
+STAGE_NAME ="Data Validation Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x')
+except Exception as e:
     raise e
